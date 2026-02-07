47c2e015b7764740d554950c90c434d9f584a29e
netteyr14 <753951852456arvin@gmail.com>
2026-02-07 21:11:04 +0800

    [DEPLOY]

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4288001544"
+    serviceWorkerVersion: "3991690489"
   }
 });
