cff66b08ee721ec797e3b6b5edf4e6060ce702ff
netteyr14 <753951852456arvin@gmail.com>
2026-08-16 04:36:16 +0800

    [DEPLOY] Fix gps error message

--- main.dart.js
+++ main.dart.js
@@ -9524,9 +9524,9 @@ a5O:function a5O(a){this.a=a},
 a5K:function a5K(a){this.a=a},
 aIM(a){return $.aHa().mm(0,a)},
 aT4(a){var s
-$label0$0:{if(B.lc===a){s="Location access is blocked. In Safari, allow Location for floodwatchph.me, then try again."
-break $label0$0}if(B.qu===a||B.qv===a||B.qx===a){s="Could not get your location. Open iPhone Settings > Privacy & Security > Location Services > Safari Websites, choose While Using the App, and turn on Precise Location, then try again."
-break $label0$0}if(B.qw===a){s="Location is unavailable in this browser. Open FloodWatch over HTTPS in Safari."
+$label0$0:{if(B.lc===a){s="Location access is blocked. Allow Location for floodwatchph.me, then try again."
+break $label0$0}if(B.qu===a||B.qv===a||B.qx===a){s="Could not get your location. Please check your device settings and make sure Location Services (or GPS) are turned on, and that this app or browser is allowed to access your precise location, then try again."
+break $label0$0}if(B.qw===a){s="Location is unavailable in this browser."
 break $label0$0}if(B.qy===a||a==null){s="Failed to get your location. Please try again."
 break $label0$0}s=null}return s},
 Qs:function Qs(a,b){this.a=a
